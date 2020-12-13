ed0e244f112cf544448ae35ad72587ea395ff56c
Daniel Davis <daniel.davis@orderbird.com>
2020-12-14 00:05:42 +0100

    embedding tiles

--- tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="rpg-tiles" tilewidth="32" tileheight="32" tilecount="1995" columns="15">
- <image source="rpg-tiles-rotated.png" width="480" height="4256"/>
-</tileset>
